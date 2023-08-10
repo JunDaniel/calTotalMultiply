668853ae46f559c507bf5c9717da60288a91b621
Jun Daniel <junfpt86@gmail.com>
2023-08-10 12:21:03 +0700

    calculator sum nany total number

--- src/CommonMethod.java
+++ src/CommonMethod.java
@@ -1,11 +1,21 @@
 public class CommonMethod {
-    int calSum (int n) {
+    int calSumWithMult (int n) {
         int totalMul = 0, multManyNumb = 1;
         for (int i=1; i<n; i++) {
              multManyNumb = multManyNumb*i;
              totalMul = multManyNumb + totalMul;
         }
         return totalMul;
+    }
+    int calSumManyTotal (int n) {
+        int result = 0, sum = 0;
+        for(int i = 1; i <= n; i++)
+        {
+            sum = sum + i;
+            result = result + sum;
+        }
+
+        return result;
     }
 }
 
--- src/main.java
+++ src/main.java
@@ -1,8 +1,12 @@
 public class main {
     public static void main(String[] args) {
         CommonMethod cmth = new CommonMethod();
-        int result = cmth.calSum(5);
+        int calSumWithMult = cmth.calSumWithMult(5);
         System.out.println("Result of S(n) = 1 + 1*2 + 1*2*3 + ... + 1*2*3*n (n > 0)");
-        System.out.println("result = " + result);
+        System.out.println("calSumWithMult = " + calSumWithMult);
+        System.out.println("==============================");
+        int calManyTotalNumb = cmth.calSumManyTotal(6);
+        System.out.println("Result of S(n) = 1 + (1+2) + (1+2+3) ... (n > 0)");
+        System.out.println("calManyTotalNumb = " + calManyTotalNumb);
     }
 }
